6e909d0db70cd81215e33a18a26de92a27c4d289
MichaelWillLee <465359180@qq.com>
2017-03-30 14:51:22 +0800

    MUE_System
    初始化代码

---
+++ MUC_System/MUC_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MUC_System.MvcApplication" Language="C#" %>
---
+++ MUC_System/MUC_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MUC_System.Startup))]
+namespace MUC_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
